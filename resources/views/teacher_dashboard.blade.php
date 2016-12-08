ca43df44749983fdb0c2a2d7307074964e251553
Rosheil Parel <rsparel@up.edu.ph>
2016-12-08 13:11:08 +0800

    UI

--- resources/views/teacher_dashboard.blade.php
+++ resources/views/teacher_dashboard.blade.php
@@ -153,11 +153,11 @@
 									</div>
 								</div>
 							</div>
-						</form>
-					</div>
 					<div class="modal-footer">
 						<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
 						<button type="submit" name="submit" class="btn btn-primary">Save changes</button>
+					</div>
+						</form>
 					</div>
 				</div>
 			</div>
